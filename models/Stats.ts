a33eed476af2bfd8eb4050bf6d7506995eb7fc86
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2025-12-05 16:41:58 +0100

    Update Stats.ts

--- models/Stats.ts
+++ models/Stats.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, model, models } from 'mongoose';
+import { Schema, model, models } from 'mongoose';
 
 const ResultSchema = new Schema({
   homeTeam: { type: String, required: true },
